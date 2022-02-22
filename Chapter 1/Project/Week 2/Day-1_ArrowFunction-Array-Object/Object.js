446cae94f4d143cfbbd4123a03c0ab60171f13e7
johnvees <yohanesvelly23@gmail.com>
2022-02-22 22:20:20 +0700

    commit week 2 day 2

--- Chapter 1/Project/Week 2/Day-1_ArrowFunction-Array-Object/Object.js
+++ Chapter 1/Project/Week 2/Day-1_ArrowFunction-Array-Object/Object.js
@@ -1,6 +1,62 @@
 // Object
 
+let biodata = {
+  firstName: 'Yohanes',
+  secondName: null,
+  lastName: 'Sabattino',
+  age: 20,
+  isMarried: false,
+  hobbies: ['makan', 'ngoding', 'surfing ig'],
+  myFullname: function () {
+    return this.firstName + this.lastName;
+  },
+};
+
 // Create
+console.log(biodata, 'ini sebelum create');
+biodata.address = 'jalan ramai sekali';
+biodata.nilai = [9, 8, 6, 5, 2];
+biodata.kriteria = { height: 172, weight: 65 };
+biodata.kriteria.bloodType = 'B+';
+console.log(biodata, 'ini setelah create');
+
+console.log(biodata.kriteria.bloodType);
+console.log(biodata['kriteria']['bloodType']);
+
 // Read
+// dot notation
+console.log(
+  biodata.firstName + biodata.lastName + 'Hobbyku' + biodata.hobbies[1]
+);
+// square notation
+console.log(
+  biodata['firstName'] + biodata['lastName'] + 'Hobbyku' + biodata['hobbies'][1]
+);
+
 // Update
+biodata.address = 'jalan baru';
+console.log(biodata);
+
 // Delete
+delete biodata.nilai;
+console.log(biodata);
+
+// Destruction pada Array dan Object
+// Array
+let students = ['Yohanes', 'Velly', 'Sabattino'];
+let [name1, name2] = students;
+name2 = 'Chico';
+console.log(name1, name2);
+
+// Object
+let { firstName, secondName, hobbies } = biodata;
+console.log(secondName, hobbies[1], 'object');
+
+// Spread Operator
+// Array
+students = ['Tsani', ...students, 'Bragi'];
+console.log(students);
+
+// Object
+const myData = { ...biodata, kelas: 'RN 1', firstName: 'Yohanes Velly' };
+console.log(myData);
---
+++ Chapter 1/Project/Week 2/Day-2_BuiltInFunction-git/script.js
@@ -0,0 +1,163 @@
+// Built-in Function atau method bawaan javascript
+
+const firstName = 'Yohanes';
+const lastName = 'Sabattino';
+const age = 21;
+
+// String
+const myName = 'Yohanes Velly Sabattino';
+const myFullName = 'Yohane, Velly, Sabattino';
+
+// slice
+const hasilSlice = myName.slice(3.1);
+console.log(hasilSlice);
+
+// concat
+// menggabungkan 2 atau lebih
+const hasilConcat = myName.concat(' adalah', ' seorang', ' kapiten');
+console.log(hasilConcat);
+
+// string literal
+// const fullName = firstName + "..." + lastName
+const fullName = `hallo nama saya ${firstName} ${lastName}, dan umur saya adalah ${age} `;
+console.log(fullName);
+
+// split
+const hasilSplit = myFullName.split('?');
+const hasilSplit1 = myName.split('');
+console.log(hasilSplit);
+console.log(hasilSplit1);
+
+// toUpperCase
+const hasilUpperCase = myName.toUpperCase();
+console.log(hasilUpperCase);
+
+// toLowerCase
+const hasilLowerCase = myName.toLowerCase();
+console.log(hasilLowerCase);
+
+// includes
+const isExist = myName.includes('Yoh');
+console.log(isExist);
+
+// Array
+const students = ['Yohanes', 'Velly', 'Sabattino'];
+
+// splice
+// const hasilSpliceArrray = students.splice(0, 2);
+// console.log(hasilSpliceArrray);
+
+// slice
+const hasilSliceArrray = students.slice(1, 2);
+console.log(hasilSliceArrray);
+
+// concat
+const hasilConcatArray = students.concat('Tsani');
+console.log(hasilConcatArray);
+
+// reverse
+const hasilReverse = students.reverse();
+console.log(hasilReverse);
+
+// join
+const hasilJoin = students.join(' ');
+console.log(hasilJoin);
+
+const reverseName = myName.split(' ').reverse().join(' ');
+console.log(reverseName);
+
+// includes
+const isExistArray = students.includes('Yoh');
+console.log(isExistArray);
+
+// foreach
+let numbers = [5, 8, 6, 9, 3, 2, 7];
+const numbersMulitpleBy3 = [];
+console.log(numbers);
+
+// const hasilForEach = numbers.forEach((isiArray, index) => {
+//   numbers[index] = isiArray * 3;
+// });
+// console.log(numbers);
+
+// map
+const hasilForEach = numbers.map((isiArray, index) => {
+  return isiArray + 2;
+});
+console.log(hasilForEach);
+
+// index of
+const indexKe = numbers.indexOf(3);
+console.log(indexKe, ' Index Ke');
+
+// sort
+const hasilSorting = numbers
+  .sort((a, b) => {
+    return a - b;
+  })
+  .reverse();
+console.log(hasilSorting, 'Hasil Sorting');
+
+// find
+const hasilFind = numbers.find((value, index) => {
+  return value < 5;
+});
+console.log(hasilFind, 'hasil find');
+
+// filter
+const hasilFilter = numbers.filter((value, index) => {
+  return value % 2 === 0;
+});
+console.log(hasilFilter, 'hasil filter');
+
+// Number
+// parseInt
+const myAge = '20';
+const hasilParseInt = parseInt(myAge);
+console.log(hasilParseInt);
+
+// parseFloat
+const resultTest = '20.5';
+const hasilParseFloat = parseFloat(resultTest);
+console.log(hasilParseFloat);
+
+// math round
+const hasilMathRound = Math.round(2.6);
+console.log(hasilMathRound);
+
+// math ceil
+const hasilMathCeil = Math.ceil(4.2);
+console.log(hasilMathCeil);
+
+// math floor
+const hasilMathFloor = Math.floor(4.9);
+console.log(hasilMathFloor);
+
+// math random
+const hasilRandom = parseInt(Math.random() * 100);
+console.log(hasilRandom);
+
+// Boolean
+console.log(!false);
+
+// membuat suatu function yang menerima 1 parameter untuk merubah huruf awal dari parameter menjadi kapital
+// rules :
+// 1. tipe data dari parameter harus string selain itu alert
+// 2. setiap kata harus berawalan kapital, hari ini hari selasa = Hari Ini Hari Selasa
+
+const changeCapital = (kalimat) => {
+  if (typeof kalimat === 'string') {
+    const array = kalimat.split(' ');
+    for (let i = 0; i < array.length; i++) {
+      array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
+      console.log(array);
+    }
+    const array2 = array.join(' ');
+    return array2;
+  } else {
+    alert(typeof kalimat);
+  }
+};
+
+console.log(changeCapital('hari ini hari selasa'));
+console.log(changeCapital('tes tes'));
