e04688b6c7c0c1d4979e065352cd6788a2a08818
johnvees <yohanesvelly23@gmail.com>
2022-03-08 21:16:20 +0700

    try to use react-native-splash-screen

--- Chapter 2/Project/Week 3/Day-3/intro/App.js
+++ Chapter 2/Project/Week 3/Day-3/intro/App.js
@@ -1,9 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import ReactNativeSplashScreen from 'react-native-splash-screen';
 import MainRouter from './src/routes/MainRouter';
 
 const App = () => {
+  useEffect(() => {
+    ReactNativeSplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <MainRouter />
