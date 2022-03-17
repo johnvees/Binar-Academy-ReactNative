3b71a8d51554e0731166f4422e818f8f2b316fe9
johnvees <yohanesvelly23@gmail.com>
2022-03-17 18:43:52 +0700

    learning about REST API and do challenge about get API

--- Chapter 3/Project/Week-5/Day-2_Promise/movie/movie/App.js
+++ Chapter 3/Project/Week-5/Day-2_Promise/movie/movie/App.js
@@ -1,80 +1,129 @@
-import {View, Text, TouchableOpacity, FlatList, Image} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  StyleSheet,
+} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {ACCESS_TOKEN, BaseUrl, ImageUrl} from './src/helpers/apiAccessTokens';
 
 export default function App() {
-  const [ListMovie, setListMovie] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [categories, setCategories] = useState('now_playing');
 
-  // useEffect(() => {
-  //   getLatestMovieList();
-  // }, []);
-
-  const getPopularMovieList = async () => {
+  const getListMovieLatest = async () => {
     try {
-      const results = await axios.get(`${BaseUrl}movie/top_rated`, {
+      setMovies('');
+      const result = await axios.get(`${BaseUrl}movie/${categories}`, {
         headers: {Authorization: `Bearer ${ACCESS_TOKEN}`},
       });
-
-      setListMovie(results.data.results);
+      setMovies(result.data.results);
     } catch (error) {
-      console.log(error);
+      return <Text>Data Tidak Ada</Text>;
     }
   };
 
-  const getTopRatedMovieList = async () => {
-    try {
-      const results = await axios.get(`${BaseUrl}movie/popular`, {
-        headers: {Authorization: `Bearer ${ACCESS_TOKEN}`},
-      });
-
-      setListMovie(results.data.results);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const getNowPlayingMovieList = async () => {
-    try {
-      const results = await axios.get(`${BaseUrl}movie/now_playing`, {
-        headers: {Authorization: `Bearer ${ACCESS_TOKEN}`},
-      });
-
-      setListMovie(results.data.results);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  useEffect(() => {
+    getListMovieLatest();
+  }, [categories]);
 
   const cardMovie = ({item}) => {
     return (
       <View>
-        <Text>{item.title}</Text>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontWeight: '900',
+            fontSize: 20,
+            color: 'black',
+          }}>
+          {item.title}
+        </Text>
         <Image
           source={{uri: `${ImageUrl}${item.poster_path}`}}
-          style={{height: 200, resizeMode: 'contain'}}
+          style={{
+            height: 200,
+            resizeMode: 'contain',
+            marginBottom: 16,
+            borderRadius: 8,
+            // backgroundColor: 'white',
+          }}
         />
       </View>
     );
   };
 
   return (
-    <View>
-      <Text>List Movie</Text>
-      <TouchableOpacity onPress={getPopularMovieList}>
-        <Text>Popular</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={getNowPlayingMovieList}>
-        <Text>Now Playing</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={getTopRatedMovieList}>
-        <Text>Top Rated</Text>
-      </TouchableOpacity>
+    <View style={{backgroundColor: 'white'}}>
+      <Text
+        style={{
+          textAlign: 'center',
+          marginVertical: 16,
+          fontSize: 24,
+          fontWeight: '900',
+          color: 'black',
+        }}>
+        List Movie
+      </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingHorizontal: 16,
+        }}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setCategories('popular');
+          }}>
+          <Text style={styles.text}>Popular</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setCategories('top_rated');
+          }}>
+          <Text style={styles.text}>Top Rated</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setCategories('now_playing');
+          }}>
+          <Text style={styles.text}>Now Playing</Text>
+        </TouchableOpacity>
+      </View>
+      <Text
+        style={{
+          marginBottom: 16,
+          marginStart: 16,
+          fontSize: 16,
+          fontWeight: '900',
+          color: 'black',
+        }}>
+        Sort By: {categories}
+      </Text>
       <FlatList
-        data={ListMovie}
+        data={movies}
         keyExtractor={(item, index) => index}
         renderItem={cardMovie}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+  },
+  button: {
+    backgroundColor: 'aqua',
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+});
---
+++ Chapter 3/Project/Week-5/Day-3_RestAPI/script.js
@@ -0,0 +1,31 @@
+console.log("Rest API")
+
+// API
+
+// Rest API
+
+// 200
+// sukses
+
+// 201
+// berhasil create
+
+// 301
+// redirect permanent atau tidak bisa kembali lagi
+
+// 302
+// redirect temporary
+
+// 307
+// redirect temporary bisa membawa data
+
+// 400
+
+// 401
+// unauthorized
+
+// 404
+// not found
+
+// > 500
+// error dari server
