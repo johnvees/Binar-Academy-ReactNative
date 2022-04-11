73323b87a9e78684d86cc9da864219f456cc445b
johnvees <yohanesvelly23@gmail.com>
2022-04-11 16:42:39 +0700

    try to learn media handling pdf

--- Chapter 5/Project/Week 9/learnTDD/App.js
+++ Chapter 5/Project/Week 9/learnTDD/App.js
@@ -1,19 +1,18 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 import React from 'react';
 import Video from 'react-native-video';
+import PDF from 'react-native-pdf';
 
 const App = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>My Video Project!</Text>
-      <Video
+      <Text>My PDF Document Project!</Text>
+      <Button title="Open PDF Document" />
+      <PDF
         source={{
-          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+          uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
         }}
-        style={{width: 300, height: 300}}
-        fullscreen={true}
-        controls={true}
-        poster="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/English_Cocker_Spaniel_4.jpg/800px-English_Cocker_Spaniel_4.jpg"
+        style={{height: 600, width: 300}}
       />
     </View>
   );
@@ -22,3 +21,16 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({});
+
+// <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//   <Text>My Video Project!</Text>
+//   <Video
+//     source={{
+//       uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+//     }}
+//     style={{width: 300, height: 300}}
+//     fullscreen={true}
+//     controls={true}
+//     poster="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/English_Cocker_Spaniel_4.jpg/800px-English_Cocker_Spaniel_4.jpg"
+//   />
+// </View>
